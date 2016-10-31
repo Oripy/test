02dd221982c5350dffdf9acd08daf22d561a6b92
Oripy <strassh@gmail.com>
2016-10-31 12:18:52 +0100

    Update server.js

--- server.js
+++ server.js
@@ -6,9 +6,16 @@ var express = require('express');
 // Create the app
 var app = express();
 
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+ 
+server.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", port " + server_port )
+});
+
 // Set up the server
 // process.env.PORT is related to deploying on heroku
-var server = app.listen(process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 3000, listen);
+var server = app.listen(process.env.OPENSHIFT_NODEJS_PORT || 8080, listen);
 
 // This call back just tells us that the server has started
 function listen() {
